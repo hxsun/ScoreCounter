0e7e279f98232d099bae1b3ce2fe2edb63fc6678
Kenneth Sun <sun.moonwell@gmail.com>
2014-10-24 19:32:58 +0800

    Initial Commit

---
+++ ScoreObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreObjc
+//
+//  Created by Kenneth Sun on 10/24/14.
+//  Copyright (c) 2014 Kenneth Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
